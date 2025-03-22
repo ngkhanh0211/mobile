f2e86e36506b5499d1b6876913cf9d4bc2750a9b
ngkhanh0211 <nguyenanhkhanh211@gmail.com>
2025-03-22 14:17:52 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import React, { useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { AppContext, AppProvider } from './context/AppContext';
-import RootRouter from './navigation/RootRouter';
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import AppNavigator from "./navigation/AppNavigator";
 
-const AppContent = () => {
-  const { isLoggedIn } = useContext(AppContext);
-
-  return <RootRouter isLoggedIn={isLoggedIn} />;
-};
-
-const App = () => (
-  <AppProvider>
-    <NavigationContainer>
-      <AppContent />
-    </NavigationContainer>
-  </AppProvider>
-);
-
-export default App;
+export default function App() {
+    return (
+        <NavigationContainer>
+            <AppNavigator />
+        </NavigationContainer>
+    );
+}
